c5bbb0bc6453ad1bda87fa0465c23d0a6d7c61d6
ivanbardon <ivanbardon@gmail.com>
2025-11-20 02:30:01 +0100

    Updates

--- assets/contacto-Dm8zrAvS.js
+++ assets/contacto-Dm8zrAvS.js
@@ -1,4 +1,4 @@
-import{c as a,j as e,M as t,P as o,b as n,r as c,L as d}from"./Layout-CRdZqLb2.js";/**
+import{c as a,j as e,M as t,P as o,b as n,r as c,L as d}from"./Layout-DGVIJ0L2.js";/**
  * @license lucide-react v0.548.0 - ISC
  *
  * This source code is licensed under the ISC license.
